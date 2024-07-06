9dfa611b63c12d3cb1305144c2822e16ab5d6f11
francoperez03 <franco.perez03@gmail.com>
2024-07-06 12:24:44 -0300

    test: created erc20 test

--- solidity/src/LoanToken.sol
+++ solidity/src/LoanToken.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Burnable, Ownable {
+contract LoanToken is ERC20, ERC20Burnable, Ownable {
     constructor(address initialOwner)
-        ERC20("MyToken", "MTK")
+        ERC20("LoanToken", "LT")
         Ownable(initialOwner)
     {}
 
---
+++ solidity/test/LoanToken.t.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import { LoanToken} from "../src/LoanToken.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TestLoanToken is Test {
+    LoanToken token;
+    address internal owner = makeAddr('owner');
+    address internal user1 = makeAddr('user');
+
+    function setUp() public {
+        token = new LoanToken(owner);
+    }
+
+    function testMint() public {
+        vm.prank(owner);
+        token.mint(user1, 1000);
+
+        assertEq(token.balanceOf(user1), 1000);
+    }
+
+    function testMintNonOwner() public {
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user1));
+
+        token.mint(user1, 1000);
+    }
+
+    function testBurn() public {
+        vm.prank(owner);
+        token.mint(user1, 1000);
+
+        vm.prank(user1);
+        token.burn(500);
+
+        assertEq(token.balanceOf(user1), 500);
+    }
+
+    function testOwnershipTransfer() public {
+        vm.prank(owner);
+        token.transferOwnership(user1);
+
+        assertEq(token.owner(), user1);
+    }
+
+    function testOwnershipTransferNonOwner() public {
+        vm.prank(user1);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user1));
+        token.transferOwnership(user1);
+    }
+}
